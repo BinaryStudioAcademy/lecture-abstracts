881e44431b8f88023eb6c8a4a5102414532ab3b7
Dmytro Kulakov <kulakovdmitr@gmail.com>
2023-02-11 14:42:44 +0200

    components import updated

---
+++ src/components/index.tsx
@@ -0,0 +1,19 @@
+export {
+  Avatar,
+  Language,
+  Accordion,
+  FacebookMessage,
+  Flute,
+  Level,
+  RadarChart,
+  ShrugMessage,
+  SlackMessage,
+  ThatsAllFolks,
+  TimelineOfContents,
+  VideoWrapper,
+} from './common';
+export { default as LecturesGrid } from './lectures-grid/LecturesGrid';
+export { default as Footer } from './footer/Footer';
+export { default as Header } from './header/Header';
+export { default as LogoLink } from './logo-link/LogoLink';
+export { Head } from './head/Head';
