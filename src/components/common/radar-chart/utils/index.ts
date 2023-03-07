17d7f19207bc48d0b952dbc9d8ead41f1426ae7f
Dmytro Kulakov <kulakovdmitr@gmail.com>
2023-03-07 13:12:23 +0200

    LA: * radar chart refactor

--- src/components/common/radar-chart/utils/index.ts
+++ src/components/common/radar-chart/utils/index.ts
@@ -1,8 +1,3 @@
-const roundToTwoDecimals = (number: number): number =>
-  +(Math.round(+(number + 'e+2')) + 'e-2');
-
-const isEqual = (a1: number[], a2: number[]): boolean => {
-  return JSON.stringify(a1) == JSON.stringify(a2);
-};
-
-export { roundToTwoDecimals, isEqual };
+export { createOptions } from './create-options/create-options';
+export { isEqual } from './is-equal/is-equal';
+export { roundToTwoDecimals } from './round-to-two-decimans/round-to-two-decimals';
