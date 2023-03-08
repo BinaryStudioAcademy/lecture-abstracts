43faed048685dd6c848f17297cbf286748081821
Dmytro Kulakov <kulakovdmitr@gmail.com>
2023-03-08 13:20:12 +0200

    LA-2: * accordion styles

--- src/components/common/accordion/accordion.tsx
+++ src/components/common/accordion/accordion.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import classNames from 'classnames';
 import * as styles from './accordion.module.scss';
 
 type Props = {
   children: [React.ReactNode, React.ReactNode];
-  grading?: boolean;
 };
 
-const Accordion: React.FC<Props> = ({ children, grading = false }) => {
+const Accordion: React.FC<Props> = ({ children }) => {
   const [heading, content] = children;
 
   return (
-    <details
-      className={classNames(styles.accordion, {
-        [styles.gradingHometask]: grading,
-      })}
-    >
-      <summary className={styles.accordionHeader}>{heading}</summary>
-      <div className={styles.accordionBody}>{content}</div>
+    <details className={styles.accordion}>
+      <summary className={styles.header}>{heading}</summary>
+      <div className={styles.content}>{content}</div>
     </details>
   );
 };
