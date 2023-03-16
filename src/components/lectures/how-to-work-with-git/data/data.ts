8299779343129214b795db1b4246300601a4ea28
Dmytro Kulakov <kulakovdmitr@gmail.com>
2023-03-16 23:57:13 +0200

    LA-18: - unused data payload

--- src/components/lectures/how-to-work-with-git/data/data.ts
+++ src/components/lectures/how-to-work-with-git/data/data.ts
@@ -1,4 +1,3 @@
 export { seoPayload } from './seo-payload';
 export { introPayload } from './intro-payload';
 export { slugPayload } from './slug-payload';
-export { timelinePayload } from './timeline-payload';
