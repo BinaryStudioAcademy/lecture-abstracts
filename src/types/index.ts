fa6e996d23aab2d57c0ae2373906a74f2da8930f
Dmytro Kulakov <kulakovdmitr@gmail.com>
2023-03-17 12:57:28 +0200

    LA-18: * radar chart types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,7 @@ export { Author } from './lectures/author/author';
 export { LectureItem } from './lectures/lecture-item/lecture-item';
 export { LecturesContext, PageContext } from './page-context/page-context';
 export { DefaultMetadata } from './meta/default-meta';
-export { ChartDatasets } from './chart/radar-chart';
+export { ChartDatasets } from './components/chart/radar-chart';
 export { SeoProps } from './seo/seo-props';
 export { MainPageContext } from './main-page/main-page-context';
 export { TimelineProps } from './components/timeline-of-contents/timeline-of-contents';
